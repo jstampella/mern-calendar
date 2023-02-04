ac01fc5715a805daa2a8079631069a8867efad02
jonathan <jona.stampella@gmail.com>
2023-02-04 19:45:44 -0300

    add config rutes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require("express");
-require("dotenv").config();
-const cors = require("cors");
-const path = require("path");
-const { dbConnection } = require("./database/config");
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const path = require('path');
+const { dbConnection } = require('./database/config');
 
 const app = express();
 
@@ -13,19 +13,22 @@ dbConnection();
 app.use(cors());
 
 // Directorio publico
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(__dirname + '/public'));
 
 //Lectura y parseo de body
 app.use(express.json());
 
 // Rutas
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/events", require("./routes/events"));
-// TODO: CRUD> Eventos
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events'));
+
+app.get('*', (req, res) => {
+	res.sendFile(__dirname + '/public');
+});
 
 const port = process.env.PORT || 3000;
 
 // Escuchar peticiones
 app.listen(port, () => {
-  console.log(`servidor corriendo en puerto ${port}`);
+	console.log(`servidor corriendo en puerto ${port}`);
 });
